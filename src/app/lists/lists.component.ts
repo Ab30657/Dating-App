07431e2fb1e833b2dd9738cf92c3ba2c81741fb0
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-05-15 00:23:07 +0545

    LikeFeature Added

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { IPagination, PaginatedResult } from '../_models/pagination';
+import { MembersService } from '../_services/members.service';
 
 @Component({
-  selector: 'app-lists',
-  templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+	selector: 'app-lists',
+	templateUrl: './lists.component.html',
+	styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+	members: Partial<Member[]>;
+	predicate = 'liked';
+	pageNumber = 1;
+	pageSize = 5;
+	pagination: IPagination;
+	constructor(private membersService: MembersService) {}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.loadLikes();
+	}
 
-  ngOnInit(): void {
-  }
+	loadLikes() {
+		this.membersService
+			.getLikes(this.predicate, this.pageNumber, this.pageSize)
+			.subscribe((response) => {
+				this.members = response.result;
+				this.pagination = response.pagination;
+			});
+	}
 
+	pageChanged(event: any) {
+		this.pageNumber = event.page;
+		this.loadLikes();
+	}
 }
