bc40f97f1c646d77905b72d2798196976835ea78
Ab30657 <amar_ronaldo30657@yahoo.com>
2021-05-14 11:08:13 +0545

    Pagination, Filtering

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -6,6 +6,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 @NgModule({
 	declarations: [],
 	imports: [
@@ -15,8 +16,9 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 		}),
 		BsDropdownModule.forRoot(),
 		TabsModule.forRoot(),
-		NgxGalleryModule,
 		BsDatepickerModule.forRoot(),
+		PaginationModule.forRoot(),
+		NgxGalleryModule,
 		FileUploadModule,
 	],
 	exports: [
@@ -26,6 +28,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 		NgxGalleryModule,
 		FileUploadModule,
 		BsDatepickerModule,
+		PaginationModule,
 	],
 })
 export class SharedModule {}
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { IPagination } from 'src/app/_models/pagination';
 import { MembersService } from '../../_services/members.service';
+import { UserParams } from '../../_models/userParams';
+import { AccountService } from '../../_services/account.service';
+import { take } from 'rxjs/operators';
+import { User } from 'src/app/_models/user';
 
 @Component({
 	selector: 'app-member-list',
@@ -9,11 +14,42 @@ import { MembersService } from '../../_services/members.service';
 	styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-	members$: Observable<Member[]>;
-
-	constructor(private memberService: MembersService) {}
+	members: Member[];
+	pagination: IPagination;
+	userParams: UserParams;
+	user: User;
+	genderList = [
+		{ value: 'male', display: 'Males' },
+		{ value: 'female', display: 'Females' },
+	];
+	constructor(
+		private memberService: MembersService,
+		private accountService: AccountService
+	) {
+		this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+			this.user = user;
+			this.userParams = new UserParams(user);
+		});
+	}
 
 	ngOnInit(): void {
-		this.members$ = this.memberService.getMembers();
+		this.loadMembers();
+	}
+
+	loadMembers() {
+		this.memberService.getMembers(this.userParams).subscribe((response) => {
+			this.members = response.result;
+			this.pagination = response.pagination;
+		});
+	}
+
+	pageChanged(event: any) {
+		this.userParams.pageNumber = event.page;
+		this.loadMembers();
+	}
+
+	resetFilters() {
+		this.userParams = new UserParams(this.user);
+		this.loadMembers();
 	}
 }
